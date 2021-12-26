c731fac865f666352cfb2279562bf5af43db7823
Brad Treloar <brad.treloar@pm.me>
2021-12-26 23:24:47 +1030

    add RecipeList component

--- src/recipes/RecipeList.test.tsx
+++ src/recipes/RecipeList.test.tsx
@@ -1,37 +1,75 @@
 import React from 'react';
 import {noop, range} from 'lodash';
-import faker from 'faker';
+// import faker from 'faker';
 import {fakeRecipe} from './factory';
-import {render, waitFor} from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  waitFor,
+  within,
+} from '@testing-library/react-native';
 import RecipeList from './RecipeList';
 
 it('displays the recipes in alphabetical order', async () => {
+  // Create a list of recipes with titles in alphabetical order.
   const testRecipes = range(3).map(index => {
     return fakeRecipe({
-      title: String.fromCharCode(index + 65) + faker.random.words(3),
+      title: `test-recipe ${String.fromCharCode(index + 65)}`,
     });
   });
 
-  const {getAllByTestId} = await waitFor(async () =>
-    render(<RecipeList recipes={testRecipes} onPress={noop} />),
+  // Pass the recipes to the RecipeList in reverse order.
+  const {getAllByText} = await waitFor(async () =>
+    render(
+      <RecipeList
+        recipes={Array.from(testRecipes).reverse()}
+        onPress={noop}
+        onLongPress={noop}
+      />,
+    ),
   );
 
-  const listItems = getAllByTestId(/recipe-list-item/i);
+  const listItems = getAllByText(/test-recipe/i);
   expect(listItems).toHaveLength(3);
   listItems.forEach((listItem, index) => {
-    const expectedTitle = testRecipes[index].title;
-    expect(listItem.children).toEqual(expectedTitle);
+    within(listItem).getByText(testRecipes[index].title);
   });
 });
 
-it('calls the onSelect handler when user presses a list item', async () => {
-  throw new Error('test not implemented');
+it('calls the onPress handler when user presses a list item', async () => {
+  const testRecipe = fakeRecipe();
+  const testOnPress = jest.fn();
+
+  // Pass the recipes to the RecipeList in reverse order.
+  const {getByText} = await waitFor(async () =>
+    render(
+      <RecipeList
+        recipes={[testRecipe]}
+        onPress={testOnPress}
+        onLongPress={noop}
+      />,
+    ),
+  );
+
+  fireEvent.press(getByText(testRecipe.title));
+  expect(testOnPress).toBeCalledWith(testRecipe);
 });
 
-it('calls the onLongPress handler when user presses a list item', async () => {
-  throw new Error('test not implemented');
-});
+it('calls the onLongPress handler when user long-presses a list item', async () => {
+  const testRecipe = fakeRecipe();
+  const testOnLongPress = jest.fn();
 
-it('calls the onDelete handler when user swipes a list item to the side', async () => {
-  throw new Error('test not implemented');
+  // Pass the recipes to the RecipeList in reverse order.
+  const {getByText} = await waitFor(async () =>
+    render(
+      <RecipeList
+        recipes={[testRecipe]}
+        onPress={noop}
+        onLongPress={testOnLongPress}
+      />,
+    ),
+  );
+
+  fireEvent(getByText(testRecipe.title), 'onLongPress');
+  expect(testOnLongPress).toBeCalledWith(testRecipe);
 });
--- src/recipes/RecipeList.tsx
+++ src/recipes/RecipeList.tsx
@@ -1,31 +1,52 @@
 import {Recipe} from '@app/types';
-import React from 'react';
+import {orderBy} from 'lodash';
+import React, {useMemo} from 'react';
 import {FlatList, Text, TouchableOpacity} from 'react-native';
 
 interface RecipeListItemProps {
   recipe: Recipe;
   onPress: (recipe: Recipe) => void;
+  onLongPress: (recipe: Recipe) => void;
 }
 
-const RecipeListItem: React.FC<RecipeListItemProps> = ({recipe, onPress}) => (
-  <TouchableOpacity onPress={() => onPress(recipe)}>
-    <Text>{recipe.title}</Text>
+const RecipeListItem: React.FC<RecipeListItemProps> = ({
+  recipe,
+  onPress,
+  onLongPress,
+}) => (
+  <TouchableOpacity
+    onPress={() => onPress(recipe)}
+    onLongPress={() => onLongPress(recipe)}>
+    <Text testID="recipe-list-item-text">{recipe.title}</Text>
   </TouchableOpacity>
 );
 
 interface RecipeSelectListProps {
   recipes: Recipe[];
   onPress: (recipe: Recipe) => void;
+  onLongPress: (recipe: Recipe) => void;
 }
 
-const RecipeList: React.FC<RecipeSelectListProps> = ({recipes, onPress}) => (
-  <FlatList
-    data={recipes.map(recipe => ({
-      recipe,
-      onPress,
-    }))}
-    renderItem={({item}) => <RecipeListItem {...item} />}
-  />
-);
+const RecipeList: React.FC<RecipeSelectListProps> = ({
+  recipes,
+  onPress,
+  onLongPress,
+}) => {
+  const sortedRecipes = useMemo(
+    () => orderBy(recipes, ['title'], ['asc']),
+    [recipes],
+  );
+
+  return (
+    <FlatList
+      data={sortedRecipes.map(recipe => ({
+        recipe,
+        onPress,
+        onLongPress,
+      }))}
+      renderItem={({item}) => <RecipeListItem {...item} />}
+    />
+  );
+};
 
 export default RecipeList;
