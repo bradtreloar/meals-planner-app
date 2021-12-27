d6581d5fa8cff3ab9ed6bc32518bcc6d1c7b75eb
Brad Treloar <brad.treloar@pm.me>
2021-12-28 00:16:42 +1030

    add floating button

---
+++ src/common/FloatingButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Button, ButtonProps} from 'react-native';
+
+const FloatingButton: React.FC<ButtonProps> = props => <Button {...props} />;
+
+export default FloatingButton;
