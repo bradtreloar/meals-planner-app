a98b8d923bfd18174ced6a4968fff6af807319ad
Brad Treloar <brad.treloar@pm.me>
2021-12-28 18:41:00 +1030

    add RecipeModal and refactor file transformer

---
+++ src/recipes/RecipeModal.test.tsx
@@ -0,0 +1,32 @@
+import {render, waitFor} from '@testing-library/react-native';
+import {noop} from 'lodash';
+import React from 'react';
+import {View} from 'react-native';
+import {fakeRecipe} from './factory';
+import RecipeModal from './RecipeModal';
+import {Recipe} from './store';
+
+interface RecipeModalFixtureProps {
+  recipe: Recipe;
+}
+
+beforeAll(() => {
+  jest.useFakeTimers();
+});
+
+const RecipeModalFixture: React.FC<RecipeModalFixtureProps> = ({recipe}) => {
+  return (
+    <View testID="recipe-modal-fixture">
+      <RecipeModal recipe={recipe} isVisible onCancel={noop} onSubmit={noop} />
+    </View>
+  );
+};
+
+it('renders', async () => {
+  const testRecipe = fakeRecipe();
+  const {getByTestId} = await waitFor(() =>
+    render(<RecipeModalFixture recipe={testRecipe} />),
+  );
+
+  getByTestId('recipe-modal');
+});
---
+++ src/recipes/RecipeModal.tsx
@@ -0,0 +1,47 @@
+import {pick} from 'lodash';
+import React, {useMemo} from 'react';
+import {View} from 'react-native';
+import Modal from 'react-native-modal';
+import RecipeForm, {RecipeFormValues} from './RecipeForm';
+import {Recipe} from './store';
+
+interface RecipeModalProps {
+  recipe?: Recipe;
+  isVisible: boolean;
+  onSubmit: (values: RecipeFormValues) => void;
+  onCancel: () => void;
+  onDelete?: () => void;
+}
+
+const RecipeModal: React.FC<RecipeModalProps> = ({
+  recipe,
+  isVisible,
+  onCancel,
+  onSubmit,
+  onDelete,
+}) => {
+  const defaultValues = useMemo(
+    () => (recipe ? pick(recipe, ['title']) : undefined),
+    [recipe],
+  );
+
+  return (
+    <Modal
+      testID="recipe-modal"
+      isVisible={isVisible}
+      hasBackdrop
+      onBackButtonPress={onCancel}
+      onBackdropPress={onCancel}>
+      <View>
+        <RecipeForm
+          defaultValues={defaultValues}
+          onSubmit={onSubmit}
+          onCancel={onCancel}
+          onDelete={onDelete}
+        />
+      </View>
+    </Modal>
+  );
+};
+
+export default RecipeModal;
