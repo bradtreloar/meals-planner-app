159dff22970f7ef2d242c1c4d47f4940a511e557
Brad Treloar <brad.treloar@pm.me>
2021-12-29 16:02:16 +1030

    add WeekSelect component

---
+++ src/meals/WeekSelect.test.tsx
@@ -0,0 +1,24 @@
+import {fireEvent, render} from '@testing-library/react-native';
+import {DateTime} from 'luxon';
+import React from 'react';
+import WeekSelect from './WeekSelect';
+
+it('advances the date by one week when the user presses the "next week" button', () => {
+  const testStartDate = DateTime.local();
+  const mockOnChange = jest.fn();
+  const {getByA11yLabel} = render(
+    <WeekSelect startDate={testStartDate} onChange={mockOnChange} />,
+  );
+  fireEvent(getByA11yLabel(/next week/i), 'onPress');
+  expect(mockOnChange).toHaveBeenCalledWith(testStartDate.plus({days: 7}));
+});
+
+it('retreats the date by one week when the user presses the "previous week" button', () => {
+  const testStartDate = DateTime.local();
+  const mockOnChange = jest.fn();
+  const {getByA11yLabel} = render(
+    <WeekSelect startDate={testStartDate} onChange={mockOnChange} />,
+  );
+  fireEvent(getByA11yLabel(/previous week/i), 'onPress');
+  expect(mockOnChange).toHaveBeenCalledWith(testStartDate.minus({days: 7}));
+});
---
+++ src/meals/WeekSelect.tsx
@@ -0,0 +1,40 @@
+import React, {useMemo} from 'react';
+import {DateTime} from 'luxon';
+import {Button, Text, View} from 'react-native';
+
+interface WeekSelectProps {
+  startDate: DateTime;
+  onChange: (newStartDate: DateTime) => {};
+}
+
+const WeekSelect: React.FC<WeekSelectProps> = ({startDate, onChange}) => {
+  const endDate = useMemo(() => startDate.plus({days: 6}), [startDate]);
+  const startDateLabel = useMemo(
+    () => startDate.toLocaleString(DateTime.DATETIME_SHORT),
+    [startDate],
+  );
+  const endDateLabel = useMemo(
+    () => endDate.toLocaleString(DateTime.DATETIME_SHORT),
+    [endDate],
+  );
+
+  return (
+    <View>
+      <Button
+        title="<"
+        accessibilityLabel="Previous week"
+        onPress={() => onChange(startDate.minus({days: 7}))}
+      />
+      <Text>
+        {startDateLabel} - {endDateLabel}
+      </Text>
+      <Button
+        title=">"
+        accessibilityLabel="Next week"
+        onPress={() => onChange(startDate.plus({days: 7}))}
+      />
+    </View>
+  );
+};
+
+export default WeekSelect;
