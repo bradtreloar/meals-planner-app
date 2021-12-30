163c295995207f1371c035844e2fba6bcb25fe02
Brad Treloar <brad.treloar@pm.me>
2021-12-31 00:12:13 +1030

    add color variables

---
+++ src/common/styles/variables.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  white: '#ffffff',
+  green: '#25a936',
+  silver: '#f8f9fa',
+  lightGrey: '#cccccc',
+  feedback: {
+    error: '#ff0000',
+    success: '#00ff00',
+    warning: '#ffff00',
+    info: '#aaaaff',
+  },
+};
